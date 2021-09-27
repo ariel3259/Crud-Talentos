ae90f1ad04c0d45798e2dbd84af12ac92476c255
ariel3259 <susanaarcia48@gmail.com>
2021-09-27 00:28:43 -0300

    se borraron las claves de seguridad

--- app.js
+++ app.js
@@ -1,29 +1,34 @@
 //declaro los modulos
-const express= require('express');
-const cors=require('cors');
-const fs=require('fs');
-const https=require('https');
+const express = require('express');
+const cors = require('cors');
+const fs = require('fs');
+const https = require('https');
 //invoco a express
-const app= express();
+const app = express();
 //invoco al puerto
-const {port}=require('./key/key');
+const { port } = require('./key/key');
 //invoco a los archivos que contienen las rutas para las peticiones 
-const cuestionarios=require('./routes/cuestionario');
-const preguntas=require('./routes/preguntas');
-const asignarPreguntas=require('./routes/cuestionariosPreguntas');
-const respuestas=require('./routes/respuestas');
-const login=require('./routes/login');
+const cuestionarios = require('./routes/cuestionario');
+const preguntas = require('./routes/preguntas');
+const asignarPreguntas = require('./routes/cuestionariosPreguntas');
+const respuestas = require('./routes/respuestas');
+const login = require('./routes/login');
 
-app.use(cors(),express.json(),express.urlencoded({extended:false}),express.static(__dirname+"/public/"));
+app.use(cors(), express.json(), express.urlencoded({ extended: false }), express.static(__dirname + "/public/"));
 
 //invoco a las rutas 
-app.use('/',cuestionarios);
-app.use('/',preguntas);
-app.use('/',asignarPreguntas);
-app.use('/',login);
-app.use('/',respuestas);
+app.use('/', cuestionarios);
+app.use('/', preguntas);
+app.use('/', asignarPreguntas);
+app.use('/', login);
+app.use('/', respuestas);
 
-https.createServer({key:fs.readFileSync('./ssl/server.key'),cert:fs.readFileSync('./ssl/server.cer')},app).listen(port||3000,err=>{
-    if(err) throw err;
-    console.log (`Funciona en el puerto:${port||3000}`);
-});
+https.createServer(
+    {
+        key: fs.readFileSync('./ssl/server.key'),
+        cert: fs.readFileSync('./ssl/server.cer')
+    }, app)
+    .listen(port || 3000, err => {
+        if (err) throw err;
+        console.log(`Funciona en el puerto:${port || 3000}`);
+    });
